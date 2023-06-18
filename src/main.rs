aee991863bd84d79b95d53554fb87e226d8ced48
ahi6 <60984726+ahi6@users.noreply.github.com>
2023-06-18 23:31:56 +0200

    Interesting "pixel blur" effect!

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     use image::io::Reader as ImageReader;
 
     let img = ImageReader::open("src/tests/images/tilemap_color_packed.png")?.decode()?;
+    let mut converted_img = DynamicImage::new_rgb8(img.width() * 2, img.height() * 2);
 
     for (x, y, px) in img.pixels() {
-        let (x, y) = (x as i32, y as i32);
         // ┌──┬──┬──┐
         // │  │A │  │
         // ├──┼──┼──┤
@@ -14,28 +14,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // ├──┼──┼──┤
         // │  │D │  │
         // └──┴──┴──┘
-        // By ChrisNF - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=71857945
+        let (x, y) = (x as i32, y as i32);
         let A = get_pixel_or_nearest(x, y + 1, &img);
         let B = get_pixel_or_nearest(x + 1, y, &img);
-        let C = get_pixel_or_nearest(x, y - 1, &img);
-        let D = get_pixel_or_nearest(x - 1, y, &img);
+        let D = get_pixel_or_nearest(x, y - 1, &img);
+        let C = get_pixel_or_nearest(x - 1, y, &img);
 
         let mut expansion = PixelExpansion::new(px);
 
-        if (C == A && C != D && A != B) {
+        if C == A && C != D && A != B {
             expansion.0 = A;
         }
-        if (A == B && A != C && B != D) {
+        if A == B && A != C && B != D {
             expansion.1 = B;
         }
-        if (D == C && D != B && C != A) {
+        if D == C && D != B && C != A {
             expansion.2 = C;
         }
-        if (B == D && B != A && D != C) {
+        if B == D && B != A && D != C {
             expansion.3 = D;
         }
+
+        // Put pixels where they belong
+        // todo: make this more better by using FOP
+        let (x, y) = ((x * 2) as u32, (y * 2) as u32);
+        converted_img.put_pixel(x, y, expansion.1);
+        converted_img.put_pixel(x + 1, y, expansion.0);
+        converted_img.put_pixel(x, y + 1, expansion.3);
+        converted_img.put_pixel(x + 1, y + 1, expansion.2);
     }
-    // img.save("src/tests/images/tilemap_color_converted.png");
+    _ = converted_img.save(r#"src/tests/images/tilemap_color_converted.png"#);
     Ok(())
 }
 
@@ -55,32 +63,26 @@ impl PixelExpansion {
 
 fn get_pixel_or_nearest(x: i32, y: i32, img: &DynamicImage) -> Rgba<u8> {
     let bounds = img.bounds(); // x, y, width, height
+    let mut coords: (u32, u32) = (0, 0);
 
-    // todo: tidy this up (extract into function perhamps?)
-    // Is x in bounds?
-    let x: u32 = if x >= 0 && img.in_bounds(x as u32, bounds.1) {
-        x as u32 // it is, keep it
+    // these two blocks do not spark joy
+    // make sure x is within bounds
+    if x < bounds.0 as i32 {
+        coords.0 = bounds.0;
+    } else if x >= bounds.2 as i32 {
+        coords.0 = bounds.2 - 1;
     } else {
-        closest_to(x, bounds.0, bounds.2) // it is not, change it
+        coords.0 = x as u32;
     };
 
-    // Is y in bounds?
-    let y: u32 = if y >= 0 && img.in_bounds(bounds.0, y as u32) {
-        y as u32 // it is, keep it
+    // make sure y is within bounds
+    if y < bounds.1 as i32 {
+        coords.1 = bounds.1;
+    } else if y >= bounds.3 as i32 {
+        coords.1 = bounds.3 - 1;
     } else {
-        closest_to(y, bounds.1, bounds.3) // it is not, change it
-    };
-
-    img.get_pixel(x, y)
-}
-
-fn closest_to(num: i32, first: u32, second: u32) -> u32 {
-    let dist_to_first = (num - first as i32).abs();
-    let dist_to_second = (num - second as i32).abs();
-
-    if dist_to_first < dist_to_second {
-        first
-    } else {
-        second
+        coords.1 = y as u32;
     }
+
+    img.get_pixel(coords.0, coords.1)
 }
