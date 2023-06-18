4e762e4e837cd481e8ed22aa7d6be11669384f2d
ahi6 <60984726+ahi6@users.noreply.github.com>
2023-06-18 23:00:02 +0200

    fail 2

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
-use image::{GenericImage, GenericImageView, Rgba};
+use image::{DynamicImage, GenericImage, GenericImageView, Rgba};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     use image::io::Reader as ImageReader;
 
     let img = ImageReader::open("src/tests/images/tilemap_color_packed.png")?.decode()?;
-    // Add 1px padding on all sides
-    let pad_amount: u32 = 1;
 
-    for (x, y, px) in img.pixels().filter(|(x, y, px)| {
-        (x < &pad_amount
-            || y < &pad_amount
-            || x > &(img.width() - pad_amount)
-            || y > &(img.width() - pad_amount))
-    }) {
+    for (x, y, px) in img.pixels() {
+        let (x, y) = (x as i32, y as i32);
         // ┌──┬──┬──┐
         // │  │A │  │
         // ├──┼──┼──┤
@@ -21,10 +15,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // │  │D │  │
         // └──┴──┴──┘
         // By ChrisNF - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=71857945
-        let A = img.get_pixel(x, y + 1);
-        let B = img.get_pixel(x + 1, y);
-        let C = img.get_pixel(x, y - 1);
-        let D = img.get_pixel(x - 1, y);
+        let A = get_pixel_or_nearest(x, y + 1, &img);
+        let B = get_pixel_or_nearest(x + 1, y, &img);
+        let C = get_pixel_or_nearest(x, y - 1, &img);
+        let D = get_pixel_or_nearest(x - 1, y, &img);
 
         let mut expansion = PixelExpansion::new(px);
 
@@ -41,7 +35,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             expansion.3 = D;
         }
     }
-
+    // img.save("src/tests/images/tilemap_color_converted.png");
     Ok(())
 }
 
@@ -56,5 +50,37 @@ struct PixelExpansion(Rgba<u8>, Rgba<u8>, Rgba<u8>, Rgba<u8>);
 impl PixelExpansion {
     fn new(px_color: Rgba<u8>) -> Self {
         PixelExpansion(px_color, px_color, px_color, px_color)
+    }
+}
+
+fn get_pixel_or_nearest(x: i32, y: i32, img: &DynamicImage) -> Rgba<u8> {
+    let bounds = img.bounds(); // x, y, width, height
+
+    // todo: tidy this up (extract into function perhamps?)
+    // Is x in bounds?
+    let x: u32 = if x >= 0 && img.in_bounds(x as u32, bounds.1) {
+        x as u32 // it is, keep it
+    } else {
+        closest_to(x, bounds.0, bounds.2) // it is not, change it
+    };
+
+    // Is y in bounds?
+    let y: u32 = if y >= 0 && img.in_bounds(bounds.0, y as u32) {
+        y as u32 // it is, keep it
+    } else {
+        closest_to(y, bounds.1, bounds.3) // it is not, change it
+    };
+
+    img.get_pixel(x, y)
+}
+
+fn closest_to(num: i32, first: u32, second: u32) -> u32 {
+    let dist_to_first = (num - first as i32).abs();
+    let dist_to_second = (num - second as i32).abs();
+
+    if dist_to_first < dist_to_second {
+        first
+    } else {
+        second
     }
 }
