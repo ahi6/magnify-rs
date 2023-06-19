b4b5d0a92f7e4ef1d0a181a42c59ab97cb95377b
ahi6 <60984726+ahi6@users.noreply.github.com>
2023-06-19 19:23:11 +0200

    refactor using FOP

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,55 @@
+#![allow(non_snake_case)]
 use image::{DynamicImage, GenericImage, GenericImageView, Rgba};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     use image::io::Reader as ImageReader;
 
-    let img = ImageReader::open("src/tests/images/tilemap_color_packed.png")?.decode()?;
+    let img = ImageReader::open(r#"src/tests/images/pbobbl2n-1s.png"#)?.decode()?;
     let mut converted_img = DynamicImage::new_rgb8(img.width() * 2, img.height() * 2);
 
-    for (x, y, px) in img.pixels() {
-        // ┌──┬──┬──┐
-        // │  │A │  │
-        // ├──┼──┼──┤
-        // │C │px│B │
-        // ├──┼──┼──┤
-        // │  │D │  │
-        // └──┴──┴──┘
-        let (x, y) = (x as i32, y as i32);
-        let A = get_pixel_or_nearest(x, y + 1, &img);
-        let B = get_pixel_or_nearest(x + 1, y, &img);
-        let D = get_pixel_or_nearest(x, y - 1, &img);
-        let C = get_pixel_or_nearest(x - 1, y, &img);
+    img.pixels()
+        .map(|(x, y, px)| {
+            let mut expansion = PixelExpansion::new(px);
+            {
+                let (x, y) = (x as i32, y as i32);
 
-        let mut expansion = PixelExpansion::new(px);
+                // ┌──┬──┬──┐
+                // │  │A │  │
+                // ├──┼──┼──┤
+                // │C │px│B │
+                // ├──┼──┼──┤
+                // │  │D │  │
+                // └──┴──┴──┘
+                let A = get_pixel_or_nearest(x, y + 1, &img);
+                let B = get_pixel_or_nearest(x + 1, y, &img);
+                let C = get_pixel_or_nearest(x - 1, y, &img);
+                let D = get_pixel_or_nearest(x, y - 1, &img);
 
-        if C == A && C != D && A != B {
-            expansion.0 = A;
-        }
-        if A == B && A != C && B != D {
-            expansion.1 = B;
-        }
-        if D == C && D != B && C != A {
-            expansion.2 = C;
-        }
-        if B == D && B != A && D != C {
-            expansion.3 = D;
-        }
+                if C == A && C != D && A != B {
+                    expansion.0 = A;
+                }
+                if A == B && A != C && B != D {
+                    expansion.1 = B;
+                }
+                if D == C && D != B && C != A {
+                    expansion.2 = C;
+                }
+                if B == D && B != A && D != C {
+                    expansion.3 = D;
+                }
+            }
 
-        // Put pixels where they belong
-        // todo: make this more better by using FOP
-        let (x, y) = ((x * 2) as u32, (y * 2) as u32);
-        converted_img.put_pixel(x, y, expansion.2);
-        converted_img.put_pixel(x + 1, y, expansion.3);
-        converted_img.put_pixel(x, y + 1, expansion.0);
-        converted_img.put_pixel(x + 1, y + 1, expansion.1);
-    }
-    _ = converted_img.save(r#"src/tests/images/tilemap_color_converted.png"#);
+            (x, y, expansion)
+        })
+        .for_each(|(x, y, expansion)| {
+            // Put the resulting pixels where they belong
+            let (x, y) = (x * 2, y * 2);
+            converted_img.put_pixel(x, y + 1, expansion.0);
+            converted_img.put_pixel(x + 1, y + 1, expansion.1);
+            converted_img.put_pixel(x, y, expansion.2);
+            converted_img.put_pixel(x + 1, y, expansion.3);
+        });
+    _ = converted_img.save(r#"src/tests/images/converted.png"#);
     Ok(())
 }
 
@@ -65,7 +71,6 @@ fn get_pixel_or_nearest(x: i32, y: i32, img: &DynamicImage) -> Rgba<u8> {
     let bounds = img.bounds(); // x, y, width, height
     let mut coords: (u32, u32) = (0, 0);
 
-    // these two blocks do not spark joy
     // make sure x is within bounds
     if x < bounds.0 as i32 {
         coords.0 = bounds.0;
